d49a8fbb177e8b0c35b8cb55916b16040a2c6d2e
Maks <maksveremey4ik@gmail.com>
2021-01-08 16:11:07 +0200

    push dz

---
+++ Homwork1/js/JS1.js
@@ -0,0 +1,21 @@
+// // HW 1 (Выведите столбец чисел от 1 до 50. Сделать 2мя способами);
+
+// вариант: 1.
+for (let i = 1; i < 51; i++) {
+	console.log(i)
+}
+
+// вариант: 2.	
+let count = 50;
+
+while (count) {
+	console.log(count--);
+}
+
+//вариант: 3.
+let i = 0;
+
+while (i<50) {
+	i++
+	console.log(i);
+}
