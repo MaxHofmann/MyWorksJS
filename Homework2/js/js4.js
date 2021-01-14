414ba6fbc151353068ef5ee84d719b5f10bb6a8f
Maks <maksveremey4ik@gmail.com>
2021-01-15 00:04:00 +0200

    Work on bugs. Correction of code and code reduction.

--- Homework2/js/js4.js
+++ Homework2/js/js4.js
@@ -1,13 +1,16 @@
 function stringValidation(userName) {
-	if (typeof userName === typeof '') {
-	} if (userName.length >= 3) {
-		return userName;
-	} if (typeof userName !== typeof '') {
-		return 'type - ' + typeof userName + '. ' + 'Ошибка, не тот тип данных';
-	} if (userName.length < 3) {
-		return 'Ошибка. Минимальная длина - 3';
-	}
+  if (userName.length >= 3) {
+    return userName;
+  }
+
+  if (typeof userName !== String) {
+    return "type - " + typeof userName + ". " + "Ошибка, не тот тип данных";
+  }
+
+  if (userName.length < 3) {
+    return "Ошибка. Минимальная длина - 3";
+  }
 }
 
-let userName = stringValidation('maks');
+let userName = stringValidation("maks");
 console.log(userName);
