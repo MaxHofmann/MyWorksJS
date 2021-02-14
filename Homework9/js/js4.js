23983c40440aaf66af4a8ccdb2fbf3277bf9006f
MaxHofmann <64605332+MaxHofmann@users.noreply.github.com>
2021-02-14 17:17:43 +0200

    Update js4.js

--- Homework9/js/js4.js
+++ Homework9/js/js4.js
@@ -4,7 +4,10 @@ function interval() {
   let from = 1;
   setTimeout(function go() {
     console.log("Прошло: " + from++ + " сек.");
-    from <= 5 ? setTimeout(go, 1000) : console.timeEnd("Прошло");
+    if(from <= 5) {
+      setTimeout(go, 1000) 
+    } else {
+      console.timeEnd("Прошло");}
   }, 1000);
 }
 interval();
